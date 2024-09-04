8f0615858b9d04f8c76d95dc8da787a5abbf0e19
the6niner <113848576+the6niner@users.noreply.github.com>
2024-09-04 09:41:51 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Handle button click
+document.getElementById('myButton').addEventListener('click', function() {
+    // Redirect to the specified URL
+    window.location.href = 'https://download.fileeagle.com/files/2023/08/GhostBrowser-2.1.5.1_x64.exe';
+});
